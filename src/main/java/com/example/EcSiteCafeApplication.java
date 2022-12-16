0bb33cbbe07f9a69d121d076a67178e757f85e63
hongo1010 <pttskp1010@gmail.com>
2022-12-16 10:44:29 +0900

    ECサイトのテンプレート

---
+++ src/main/java/com/example/EcSiteCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcSiteCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcSiteCafeApplication.class, args);
+	}
+
+}
